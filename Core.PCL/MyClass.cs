b4923b7a58164dbbe97cecd4164bd33f3f730492
Daniele Davì <ddavi@MacBook-di-DD.local>
2016-06-15 22:39:52 +0200

    first

---
+++ Core.PCL/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.PCL
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
---
+++ Core.iOS/MyClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Core.iOS
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+	}
+}
+
